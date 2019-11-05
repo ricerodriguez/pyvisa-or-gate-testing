4aa137667fa637a4189250671ddb685e043dd81e
Rice Rodriguez <rice.rodriguez@ttu.edu>
2019-11-05 10:13:09 -0600

    Initial directory set up for files

---
+++ src/power_consumption_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+'''
+Description: 
+    Power Consumption Test finds the worst-case power consumption for
+    both static and dynamic operational conditions.
+
+Procedure:
+    1. Set all device output pins to be open circuit.
+    2. Set the power supply pin to its recommended V_CC.
+    3. Measure the current draw I_CC from the power supply pin.
+'''
+__author__ = "Victoria (Rice) Rodriguez, Isaac Morales"
+__email__ = "rice.rodriguez@ttu.edu, isaac.morales@ttu.edu"
+__status__ = "Prototype"
+
+
