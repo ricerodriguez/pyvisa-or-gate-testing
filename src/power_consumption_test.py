0665137e8a90f1e57f4af95e349f399ffab23a49
Rice Rodriguez <rice.rodriguez@ttu.edu>
2019-11-05 13:46:22 -0600

    Missed a parenthesis

--- src/power_consumption_test.py
+++ src/power_consumption_test.py
@@ -21,7 +21,7 @@ class PowerConsumptionTest:
     def __init__(self):
         rm = pyvisa.ResourceManager()
         self.list_resources = rm.list_resources()
-        logging.info('All resources:\n',str(self.list_resources)
+        logging.info('All resources:\n',str(self.list_resources))
         self.msg = 'Please disconnect all output pins from the DUT and connect the SMU to the VCC pin.'
         self.smu = None
         self.__config_instr(rm)
