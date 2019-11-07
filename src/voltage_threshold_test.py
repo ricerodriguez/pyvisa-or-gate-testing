fa1988c15afdec6b36ed8ef95a7eb3765c002d39
isaac <isaacmorales3215@gmail.com>
2019-11-07 13:20:38 -0600

    take two.

--- src/voltage_threshold_test.py
+++ src/voltage_threshold_test.py
@@ -21,8 +21,8 @@ Procedure:
     6. If the device produces and error, record the input voltage as Vih
 
 '''
-__author__ = "Victoria (Rice) Rodriguez, Isaac Morales"
-__email__ = "rice.rodriguez@ttu.edu, isaac.morales@ttu.edu"
+__author__ = "Isaac Morales"
+__email__ = "isaac.morales@ttu.edu"
 __status__ = "Prototype"
 
 
@@ -33,29 +33,40 @@ import argparse as arg
 class VoltageThreshold:
     def __init__(self):
         self.rm = pyvisa.ResourceManager()
-        self.rm.list.resources()
+        self.list_resources = self.rm.list_resources()
+        logging.info('All resources:\n',pprint.pformat(self.list_resources)
+        self,msg = 'Please make sure you connected one input pin and one output pin'
         self.smu = None
+        self.res = None
         self.__smuConfig(self)
 
     def __smuConfig(self):
         try:
-            self.smu = rm.open_resource('')[0]
+            self.smu = self.rm.open_resource(self.list_resources[-1])
             self.smu.read_termination = '\n'
             self.smu.write_termination = '\n'
             logging.debug('set SMU as {}'.format(self.smu))
-            self.smu.query('*IDN')
+            bytes_back = self.smu.query('*IDN?')
             logging.info('Queried SMU for IDN and received the following message back: \n',
                         self.smu.read_bytes(1))
 
         except Exception as err:
+            logging.erroer(err)
             logging.error('Make sure everything is connected correctly! :)')
             exit()
-'''
+
+        except pyvisa.errors.VisaIOerror as err:
+            logging,error(err)
+            exit(-1)
+
     def findThresh(self, Vcc, pinStart):
-        pinOut = i'm sure theres a way to read this pin value as a bool value
+        pinOut = 0
         Vin = 0
-        if pinStart:
-            Vin = Vcc
+            #reset,set to preset,clear
+        self.smu.write('*rst;outp off;*cls')
+        if pinStart:    #if we're starting at 1
+            Vin = Vcc      #slowly move our way down from VCC
+            self.smu.write('sour:func:mode: volt')
             for thresh in range(Vin, 0, -.1):
                 if pinOut == 0:
                     return thresh
@@ -68,7 +79,7 @@ class VoltageThreshold:
                     return thresh
                 else:
                     continue
-'''
+
 
 if __name__ == '__main__':
     parser = arg.ArgumentParser(description = 'Voltage Threshold Test finds the minimum input Voltage'
@@ -82,7 +93,7 @@ if __name__ == '__main__':
     else:
         logging.basicConfig(level=logging.WARNING)
 
-    Vcc = float(input('What is the Vcc of your device? \n'))
+    Vcc = input('What is the Vcc of your device? \n')
     pinStart = bool(input('What are you starting the input pin at? 1 or 0 \n'))
 
 
