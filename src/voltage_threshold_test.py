6f1db14fbe459d2e947677ba2f7accce9855e373
isaac <isaacmorales3215@gmail.com>
2019-11-12 18:36:51 -0600

    Included Ian's things...Rice we need to talk about this

--- src/voltage_threshold_test.py
+++ src/voltage_threshold_test.py
@@ -5,7 +5,7 @@ Description:
     The Voltage Threshold test determines the minimum input Voltage
     required to cause the device output to switch from high to low
 
-Procedure:
+Procedures:
     1. Write an input pattern to the device that results in a logic 0 on
     the input pin under test and also propagates that 0 across to an output pin
 
@@ -30,42 +30,90 @@ import pyvisa
 import logging
 import argparse as arg
 from resources import SMUSetup
+from time import sleep
 
 class VoltageThreshold:
     def __init__(self):
         self.rm = pyvisa.ResourceManager()
-        self.list_resources = self.rm.list_resources()
-        logging.info('All resources:\n',pprint.pformat(self.list_resources)
-        self,msg = 'Please make sure you connected one input pin and one output pin'
+        self.msg = 'Please make sure you connected one input pin and one output pin'
         self.instr = SMUSetup('volt', 0 ,'volt')
         self.smu = self.instr.smu
+        self.smu.find_arduino(6)
         self.res = None
 
 
-    def execute_test(self, Vcc, pinStart):
-        pinOut = 0
-        Vin = 0
-            #reset,set to preset,clear
-        self.smu.write('*rst;outp off;*cls')
-        if pinStart:    #if we're starting at 1
-            Vin = Vcc      #slowly move our way down from VCC
-            self.smu.write('sour:func:mode: volt')
-            for thresh in range(Vin, 0, -.1):
-                self.smu.setup('volt', thresh, 'curr')
-                self.smu.write('outp on')
-                if pinOut == 0: 
-                    return thresh
-                else:
-                     continue
+    def execute_test(self, Vcc, pinStart, pinPos):
+#         pinOut = 0
+#         Vin = 0
+#             #reset,set to preset,clear
+#         self.smu.write('*rst;outp off;*cls')
+#         if pinStart:    #if we're starting at 1
+#             Vin = Vcc      #slowly move our way down from VCC
+#             self.smu.write('sour:func:mode: volt')
+#             for thresh in range(Vin, 0, -.1):
+#                 self.smu.setup('volt', thresh, 'curr')
+#                 self.smu.write('outp on')
+#                 if pinOut == 0:
+#                     return thresh
+#                 else:
+#                      continue
+#         else:
+#             Vin = 0
+#             for thresh in range(Vin, Vcc, .1):
+#                 self.smu.setup('volt', thresh, 'curr')
+#                 self.smu.write('outp on')
+#                 if pinOut == 1:
+#                     return thresh
+#                 else:
+#                     continue
+# 9225 for a
+# 18450 for b
+        self.smu.Multirelay(pinPos)
+        thresh_0, thresh_1, thresh_2, thresh_3 = 0
+        if pinStart:
+            start = Vcc * 10 + 1
+            finish = 0
+            step = -1
+            readOut = 0
         else:
-            Vin = 0
-            for thresh in range(Vin, Vcc, .1):
-                self.smu.setup('volt', thresh, 'curr')
-                self.smu.write('outp on')
-                if pinOut == 1:
-                    return thresh
-                else:
-                    continue
+            start = 0
+            finish = Vcc * 10 + 1
+            step = 1
+            readOut = 1
+
+        for volts in range(start, finish, step):
+            self.smu.setup('volt', volts/10, 'curr')
+            self.smu.write('outp on')
+            sleep(.025)
+            read = int(self.smu.read_inputs(), base = 2)
+            print(volts,read)
+            if read & 0b0001 != readOut and thresh_0 == 0:
+                thresh_0 = volts
+            if read & 0b0010 != readOut << 1 and thresh_1 == 0:
+                thresh_1 = volts
+            if read & 0b0100 != readOut << 2 and thresh_2 == 0:
+                thresh_2 = volts
+            if read & 0b1000 != readOut << 3 and thresh_3 == 0:
+                thresh_3 = volts
+
+        return thresh_0, thresh_1, thresh_2, thresh_3
+
+    def document(self,info):
+        testNum = input('is this the first chip? y/n ')
+
+        if testNum == 'y':
+            reults = open("VoltageThresholdTest.txt","w")
+            results
+            results.write("Testing chip number: " + testNum)
+            logging.debug('chip number: ' + testNum)
+        else:
+            results = open("VoltageThresholdTest.txt","a")
+            results.write("Testing chip number: " + testNum)
+            logging.debug('chip number: ' + testNum)
+
+        results.write('\n {} \n'.format(info))
+        results.close()
+
 
 
 if __name__ == '__main__':
@@ -82,8 +130,15 @@ if __name__ == '__main__':
 
     Vcc = input('What is the Vcc of your device? \n')
     pinStart = bool(input('What are you starting the input pin at? 1 or 0 \n'))
+    choice = input('are you testing pins A or B?')
+    if choice == 'A':
+        pinPos = 9225
+    elif choise == 'B':
+        pinPos = 18450
+
 
 
     vt = VoltageThreshold()
-    Threshold =  vt.execute_test(Vcc,pinStart)
+    Thresholds =  vt.execute_test(Vcc,pinStart,pinPos)
+    vt.execute_test(Thresholds)
     # print('Threshold')
