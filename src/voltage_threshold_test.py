e3a884659612cd066dad597f059364b9e3ba5168
Victoria Rodriguez <31574111+ricerodriguez@users.noreply.github.com>
2019-11-15 23:50:57 -0600

    Fixed errors

--- src/voltage_threshold_test.py
+++ src/voltage_threshold_test.py
@@ -41,7 +41,7 @@ from resources import SMUSetup, RelayBoard
 from time import sleep
 
 class VoltageThreshold:
-    def __init__(self, relay, pins = [None*16]):
+    def __init__(self, relay, pins):
         self.rm = pyvisa.ResourceManager()
         self.msg = 'Please make sure you connected one input pin and one output pin'
         self.instr = SMUSetup('volt', 0 ,'volt')
@@ -76,7 +76,7 @@ class VoltageThreshold:
                 compare = 8 # Vil
             for volts in range(start, finish, step):
                 #write the new voltage to the input pin we're working with
-                self.smu.setup('volt', volts/10, 'curr')
+                self.instr.setup('volt', volts/10, 'curr')
                 self.smu.write('outp on')
                 #wait a bit before reading the result at the output
                 sleep(.025)
@@ -94,7 +94,7 @@ class VoltageThreshold:
                         # self.rm.close()
                         self.meas['pin {}'.format(currPin)][1] = thresh   #return the threshold for this OR gate
                         #if we are stepping up and our threshold passed
-                    elif !pinStart and volts > compare:
+                    elif not pinStart and volts > compare:
                         self.outcome = True #then the test was successful, we passed
                         # self.rm.close()
                         self.meas['pin {}'.format(currPin)][0] = thresh  #return the threshold for this OR gate
@@ -120,14 +120,12 @@ if __name__ == '__main__':
     else:
         logging.basicConfig(level=logging.WARNING)
 
-    Vcc = input('What is the Vcc of your device? \n')
+    Vcc = int(input('What is the Vcc of your device? \n'))
     pinStart = bool(input('Are starting with an output of 1 or 0? \n'))
-    choice = input('are you testing pins A or B? \n')
 
 
 
-
-    vt = VoltageThreshold()
-    Threshold =  vt.execute_test(Vcc,pinStart)
-    vt.execute_test(Thresholds)
+    relay = RelayBoard()
+    vt = VoltageThreshold(relay,range(1,14))
+    vt.execute_test(Vcc,pinStart)
     # print('Threshold')
